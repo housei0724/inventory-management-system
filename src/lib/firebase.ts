0b824559e6558b3ba28fe3e1c7910e0d27dd7c72
housei <elninhokknn@gmail.com>
2026-06-18 09:24:14 +0900

    initial commit

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDJvxDaCHGY3IXgD9eXU0lyCc5vTcrooeU",
-    authDomain: "inventory-app-14768.firebaseapp.com",
-    projectId: "inventory-app-14768",
-    storageBucket: "inventory-app-14768.firebasestorage.app",
-    messagingSenderId: "856844157352",
-    appId: "1:856844157352:web:0545e5469c5ab0006add9d"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized already
